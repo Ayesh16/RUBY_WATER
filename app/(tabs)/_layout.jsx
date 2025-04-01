3ebee6cea5f7646cdac6eaeece6e5cebd92d836c
Ayesh16 <ayeshaayesha16.2003@gmail.com>
2025-04-01 11:37:40 +0530

    ui

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,37 +1,37 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { FontAwesome } from '@expo/vector-icons';
+// import { Tabs } from 'expo-router';
+// import React from 'react';
+// import { FontAwesome } from '@expo/vector-icons';
 
-export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarShowLabel: true }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="home" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="info-circle" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="contact"
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="phone" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
-}
+// export default function TabLayout() {
+//   return (
+//     <Tabs screenOptions={{ tabBarShowLabel: true }}>
+//       <Tabs.Screen
+//         name="home"
+//         options={{
+//           headerShown: false,
+//           tabBarIcon: ({ color, size }) => (
+//             <FontAwesome name="home" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="about"
+//         options={{
+//           headerShown: false,
+//           tabBarIcon: ({ color, size }) => (
+//             <FontAwesome name="info-circle" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="contact"
+//         options={{
+//           headerShown: false,
+//           tabBarIcon: ({ color, size }) => (
+//             <FontAwesome name="phone" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
